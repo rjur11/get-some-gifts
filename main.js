68232c1bc3146b4c3cbef281fb0053fdc71c2d59
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-12-21 17:04:34 -0500

    Render screen

--- main.js
+++ main.js
@@ -1,22 +1,38 @@
 fetch("https://mysterious-mesa-00016.herokuapp.com/items")
   .then((response) => response.json())
-  .then((data) => {
-    let htmlBody = document.querySelector("tbody");
-    let ourData = data.map((person) => {
-      return `
-        <tr>
-              <td>${person.recipient}</td>
-              <td>${person.name}</td>
-              <td>$${person.priceInDollars}</td>
-              <td><input type="checkbox"></td>
-        </tr>
-            `;
-    });
-    htmlBody.innerHTML = ourData.join("");
-    let span = document.querySelector("span");
-    let totalCost = data.reduce((acc, gift) => {
-      return acc + gift.priceInDollars;
-    }, 0);
-    span.innerText = `$${totalCost}`;
-  })
+  .then((data) => renderScreen(data))
   .catch((error) => console.log(error));
+
+function renderScreen(data) {
+  const tbody = document.querySelector("tbody");
+  tbody.innerHTML = "";
+  const span = document.querySelector("span");
+  let totalPrice = data.reduce((acc, gift) => {
+    return acc + gift.priceInDollars;
+  }, 0);
+  data
+    .map((person) => {
+      const row = document.createElement("tr");
+      row.innerHTML = `<td>${person.recipient}</td>
+        <td>${person.name}</td>
+        <td>$${person.priceInDollars}</td>`;
+      const lastCell = document.createElement("td");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.addEventListener("click", (e) => {
+        if (e.target.checked) {
+          totalPrice -= person.priceInDollars;
+        } else {
+          totalPrice += person.priceInDollars;
+        }
+        span.innerText = "$" + totalPrice.toFixed(2);
+      });
+      lastCell.appendChild(checkbox);
+      row.appendChild(lastCell);
+      return row;
+    })
+    .forEach((row) => {
+      tbody.appendChild(row);
+    });
+  span.innerText = "$" + totalPrice;
+}
