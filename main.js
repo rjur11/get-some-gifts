3b568a5c8aca71fb102ee8bd3ef663c43eaaeb54
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-12-21 16:28:51 -0500

    Add cells

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
-import "./styles.css";
-imports { fetchGifts } from "./apiCalls.js";
+fetch("https://mysterious-mesa-00016.herokuapp.com/items")
+  .then((response) => response.json())
+  .then((data) => {
+    let htmlBody = document.querySelector("tbody");
+    let ourData = data.map((person) => {
+      return `
+        <tr>
+              <td>${person.recipient}</td>
+              <td>${person.name}</td>
+              <td>$${person.priceInDollars}</td>
+              <td><input type="checkbox"></td>
+        </tr>
+            `;
+    });
+    htmlBody.innerHTML = ourData.join("");
+    let span = document.querySelector("span");
+    let totalCost = data.reduce((acc, gift) => {
+      return acc + gift.priceInDollars;
+    }, 0);
+    span.innerText = `$${totalCost}`;
+  })
+  .catch((error) => console.log(error));
